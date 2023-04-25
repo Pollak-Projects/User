0ef0ca7d555074a80e36f6ce30ee405891e7970a
TGyAkos <85934789+TGyAkos@users.noreply.github.com>
2023-04-25 21:31:33 +0200

    Updated global_connect

--- app/global/database_connection/global_connect.php
+++ app/global/database_connection/global_connect.php
@@ -1,17 +1,19 @@
 <?php
 class DatabaseConnect
 {
-    public function ConnectToDatabaseTable(string $UserProvidedDatabase)
+    public function ConnectToDatabaseTable(string $UserProvidedTable)
     {
-        $mysqli = new mysqli("localhost", "tanulo", "tanulo", $UserProvidedDatabase);
+        $mysqli = new mysqli("localhost", "tanulo", "tanulo", "Bluementa");
 
         if (!$mysqli) {
             die('Unable to coonnect to the MySql server' . $mysqli->connect_errno);
         }
-        $db = $mysqli->select_db($UserProvidedDatabase);
+        $db = $mysqli->select_db($UserProvidedTable);
         if (!$db) {
             die('Unable to coonnect to the MySql server' . $mysqli->connect_errno);
         }
         @$mysqli->query('SET NAMES utf8mb4');
+
+        return $mysqli;
     }
 }
